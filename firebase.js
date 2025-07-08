7b5fc25a0dc992b95910a2eb3ef80aab368cc405
egaziozbek <egaziozbek@gmail.com>
2025-07-08 18:28:59 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
-export const firebaseApp = {}; // örnek firebase yapılandırması
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLp-t81vU5S1LDr76YKszeG1LcHwU0GZQ",
+  authDomain: "okuma-testi.firebaseapp.com",
+  projectId: "okuma-testi",
+  storageBucket: "okuma-testi.firebasestorage.app",
+  messagingSenderId: "820936161658",
+  appId: "1:820936161658:web:03f8891fa43044d4667ec3",
+  measurementId: "G-TTKE36VBT9"
+};
+
+const app = initializeApp(firebaseConfig);
+const firebaseAuth = getAuth(app);
+
+export { firebaseAuth, RecaptchaVerifier, signInWithPhoneNumber };
