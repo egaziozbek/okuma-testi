1b1afee9a973b63e9333fc10e63e86399b24c31a
egaziozbek <egaziozbek@gmail.com>
2025-07-09 15:45:00 +0300

    Update admin-login-debug.js

--- admin-login-debug.js
+++ admin-login-debug.js
@@ -4,6 +4,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { firebaseConfig } from "./firebase-config.js";
 
+
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
