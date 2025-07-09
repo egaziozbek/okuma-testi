5c85fe5266766d3cca42c7cb38e2eb97a16178e1
egaziozbek <egaziozbek@gmail.com>
2025-07-09 15:17:48 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
 
 // Senin Firebase yapılandırman
 const firebaseConfig = {
-  apiKey: "AIzaSyCLp-t81vU5S1LDr76YKszeG1LcHwU0GZQ",
+   apiKey: "AIzaSyCLp-t81vU5S1LDr76YKszeG1LcHwU0GZQ",
   authDomain: "okuma-testi.firebaseapp.com",
   projectId: "okuma-testi",
   storageBucket: "okuma-testi.firebasestorage.app",
   messagingSenderId: "820936161658",
-  appId: "1:820936161658:web:03f8891fa43044d4667ec3",
-  measurementId: "G-TTKE36VBT9"
+  appId: "1:820936161658:web:0bd9c4dc043cfad0667ec3",
+  measurementId: "G-2WBL96ETV1"
 };
 
 // Uygulama başlatılıyor
