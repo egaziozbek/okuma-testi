7083b5cd52c57152430425794abfaf33001af24a
egaziozbek <egaziozbek@gmail.com>
2025-07-09 15:52:28 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,7 +7,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCLp-t81vU5S1LDr76YKszeG1LcHwU0GZQ",
   authDomain: "okuma-testi.firebaseapp.com",
   projectId: "okuma-testi",
-  storageBucket: "okuma-testi.appspot.com",
+  storageBucket: "okuma-testi.firebasestorage.app",
   messagingSenderId: "820936161658",
   appId: "1:820936161658:web:0bd9c4dc043cfad0667ec3",
   measurementId: "G-2WBL96ETV1"
